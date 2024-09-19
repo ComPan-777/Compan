38decbae1f4f9dd311ab2ed4407f7b59cf14ce8c
Subash Jirel <subashjrl@gmail.com>
2024-09-19 17:42:06 +0545

    initialized files

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  //   basePath: '/compan', // change this pachi//https://www.freecodecamp.org/news/how-to-deploy-next-js-app-to-github-pages/#:~:text=GitHub%20Pages%20is%20designed%20to,mode%20to%20export%20inside%20next.
+  output: 'export', // <=== enables static exports
+  reactStrictMode: true,
+};
+
+export default nextConfig;
